5bba33a66edd984cc11e8a8e849bc04b7e7c8b08
WDavidPj <wilsonpadilla2014@gmail.com>
2019-05-04 23:37:04 -0500

    forms

--- form/src/App.js
+++ form/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './css/index.css';
+//Components
+import Inputs from './components/Inputs'
 
 export default class App extends Component {
   render(){
     return (
       <div className="App">
-        
+        <div>
+          <Inputs/>
+        </div>
       </div>
     );
   }
